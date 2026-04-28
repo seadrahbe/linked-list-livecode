1bee5c527378a976f4cfadb62a2e738a67789184
seadrahbe <174991882+seadrahbe@users.noreply.github.com>
2026-04-28 11:56:18 -0700

    Practice

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,9 @@
 public class Node {
-    
+  public char value;
+  public Node next;
+
+  public Node(char value, Node next) {
+    this.value = value;
+    this.next = next;
+  }
 }
--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,21 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("~linked lists practice~");
+        Node myNode = new Node('w', null);
+        Node otherNode = new Node('x', null);
+        Node coolNode = new Node('e', null);
+
+        myNode.next = otherNode;
+        otherNode.next = coolNode;
+
+        Node current = myNode;
+
+        while(current != null) {
+            System.out.println(current.value);
+            current = current.next;
+        }
+
+        System.out.println(myNode.value);
+        System.out.println(myNode.next.value);
+        System.out.println(myNode.next.next.value);
     }
 }
